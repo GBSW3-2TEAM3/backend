078383a301413e3bd52b8363a8af841ad97197c8
sanghyeon lim <i48281973@gmail.com>
2025-04-11 13:32:07 +0900

    Update AuthenticationResponse.java

--- src/main/java/com/example/walkinggo/dto/AuthenticationResponse.java
+++ src/main/java/com/example/walkinggo/dto/AuthenticationResponse.java
@@ -1,14 +1,19 @@
 package com.example.walkinggo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @AllArgsConstructor
 public class AuthenticationResponse {
-    @Schema(description = "JWT 토큰 (로그인 성공 시) 또는 사용자 아이디 (회원가입 성공 시)")
+
+    @Schema(description = "JWT 토큰")
     private final String jwt;
+
     @Schema(description = "로그인 성공 여부")
     private final boolean isLoggedIn;
+
+    @Schema(description = "로그인한 사용자 아이디")
+    private final String username;
 }
