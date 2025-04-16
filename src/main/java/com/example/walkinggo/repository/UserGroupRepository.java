ed401e4902400f5022569099787363686bd2d786
sanghyeon lim <i48281973@gmail.com>
2025-04-16 10:10:51 +0900

    Create UserGroupRepository.java

---
+++ src/main/java/com/example/walkinggo/repository/UserGroupRepository.java
@@ -0,0 +1,24 @@
+package com.example.walkinggo.repository;
+
+import com.example.walkinggo.entity.UserGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
+
+    Optional<UserGroup> findByParticipationCode(String participationCode);
+
+    List<UserGroup> findByIsPublicTrueOrderByNameAsc();
+
+    boolean existsByParticipationCode(String participationCode);
+
+    @Query("SELECT CASE WHEN COUNT(ug) > 0 THEN true ELSE false END " +
+            "FROM UserGroup ug JOIN ug.members m " +
+            "WHERE ug.id = :groupId AND m.username = :username")
+    boolean isUserMemberOfGroup(Long groupId, String username);
+
+    List<UserGroup> findByNameContainingIgnoreCaseAndIsPublicTrue(String name);
+}
