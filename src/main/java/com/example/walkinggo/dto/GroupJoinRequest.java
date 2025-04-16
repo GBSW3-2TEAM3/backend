3ab528d73e05f81478bb72c006aae71c8ff611ff
sanghyeon lim <i48281973@gmail.com>
2025-04-16 10:10:26 +0900

    Create GroupJoinRequest.java

---
+++ src/main/java/com/example/walkinggo/dto/GroupJoinRequest.java
@@ -0,0 +1,15 @@
+package com.example.walkinggo.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GroupJoinRequest {
+
+    @NotBlank(message = "참가 코드를 입력해주세요.")
+    @Schema(description = "비공개 그룹 참가 코드", example = "AB12CD")
+    private String participationCode;
+}
