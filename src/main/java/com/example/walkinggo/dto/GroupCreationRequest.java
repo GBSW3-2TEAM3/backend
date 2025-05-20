eee105e9e30318f90793aeae38b099d36334be59
sanghyeon lim <i48281973@gmail.com>
2025-05-20 09:55:40 +0900

    Update GroupCreationRequest.java

--- src/main/java/com/example/walkinggo/dto/GroupCreationRequest.java
+++ src/main/java/com/example/walkinggo/dto/GroupCreationRequest.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 public class GroupCreationRequest {
 
     @NotBlank(message = "그룹 이름은 필수입니다.")
-    @Size(min = 2, max = 100, message = "그룹 이름은 2자 이상 100자 이하로 입력해주세요.")
+    @Size(min = 2, max = 20, message = "그룹 이름은 2자 이상 20자 이하로 입력해주세요.")
     @Schema(description = "생성할 그룹 이름", example = "우리 동네 걷기 모임")
     private String name;
 
@@ -25,8 +25,8 @@ public class GroupCreationRequest {
     @Schema(description = "그룹 공개 여부 (true: 공개, false: 비공개)", example = "true")
     private Boolean isPublic;
 
-    @Schema(description = "비공개 그룹 생성 시 사용할 팀 코드 (숫자만, 비공개 그룹인 경우 필수)", example = "123456")
-    @Pattern(regexp = "^[0-9]+$", message = "팀 코드는 숫자만 입력 가능합니다.") // 숫자만
-    @Size(min = 4, message = "팀 코드는 4자리 이상 10자리 이하이어야 합니다.")
+    @Schema(description = "비공개 그룹 생성 시 사용할 팀 코드 (숫자로만 구성된 문자열, 비공개 그룹인 경우 필수)", example = "123456")
+    @Pattern(regexp = "^[0-9]+$", message = "팀 코드는 숫자만 입력 가능합니다.")
+    @Size(min = 4, max = 16, message = "팀 코드는 4자리 이상 16자리 이하이어야 합니다.")
     private String participationCode;
 }
