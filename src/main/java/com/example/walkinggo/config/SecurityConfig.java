51d8499bbe75fd5edb9be472c708f73942e4673b
sanghyeon lim <i48281973@gmail.com>
2025-06-18 08:42:52 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/walkinggo/config/SecurityConfig.java
+++ src/main/java/com/example/walkinggo/config/SecurityConfig.java
@@ -3,19 +3,19 @@ package com.example.walkinggo.config;
 import com.example.walkinggo.filter.JwtRequestFilter;
 import com.example.walkinggo.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -48,7 +48,9 @@ public class SecurityConfig {
                                 AntPathRequestMatcher.antMatcher("/swagger-ui/**"),
                                 AntPathRequestMatcher.antMatcher("/api-docs/**"),
                                 AntPathRequestMatcher.antMatcher("/webjars/**"),
-                                AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/share/**")
+                                AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/share/**"),
+                                AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/api/walk-logs/recommended"),
+                                AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/api/walk-logs/*/details")
                         ).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.OPTIONS, "/**")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).authenticated()
